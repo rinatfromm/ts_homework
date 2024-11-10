e84c5f9b49a4f6d234b1022957d600732766967c
Rinat Kov <frommrinat@gmail.com>
2024-11-10 13:42:15 +0100

    Реализовал функцию normalizeData для нормализации сообщений.

--- index.ts
+++ index.ts
@@ -19,7 +19,7 @@ const totalPrice = ({ price, discount, isInstallment, months }: TotalPriceParams
 const price = totalPrice({ price: 100000, discount: 25, isInstallment: true, months: 12 });
 
 
-
+console.log(price)
 
 
 
